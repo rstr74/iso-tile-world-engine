c1af47b1548f99057d8dfbe8927024d94c634de2
rstr74 <robbert@buiswerk.info>
2012-05-30 11:16:35 +0200

    chrome diagonal direction scroll issue (jquery css update now is one command)

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,81 +1,77 @@
 $(function() {
-	$("#isoWorldContainer").isoWorldEngine({
-		logging : false,
-		sound : false,
-		cropH : 10,
-		cropW : 10,
-		fps : 60,
-		showObjectBorders : false,
-		showTicker:true,
-		tmxFilePath : "world/",
-		tmxFile : "map.tmx"
-	});
+    $("#isoWorldContainer").isoWorldEngine({
+        logging: false,
+        sound: false,
+        cropH: 10,
+        cropW: 10,
+        fps: 60,
+        showObjectBorders: false,
+        showTicker: true,
+        tmxFilePath: "world/",
+        tmxFile: "map.tmx"
+    });
 
-	$("#isoWorldContainer").isoWorldEngine.onReady = function() {
-	};
-	var dx = 0;
-	var speed = 5;
-	var keyControl = new $.fn.isoWorldEngine.KeyControl();
-	$("#tileAndObjectsDummy").css("top", 40);
-	$("#isoWorldContainer").isoWorldEngine.update = function() {
-		var direction = keyControl.getDirection();
-		//$.fn.isoWorldEngine.drawText(10, 40, direction);
+    $("#isoWorldContainer").isoWorldEngine.onReady = function() {
+        };
+    var dx = 0;
+    var speed = 5;
+    var keyControl = new $.fn.isoWorldEngine.KeyControl();
+    $("#tileAndObjectsDummy").css("top", 40);
+    $("#isoWorldContainer").isoWorldEngine.update = function() {
+        var direction = keyControl.getDirection();
+        $.fn.isoWorldEngine.drawText(10, 40, direction);
 
-		switch (direction) {
-			case 8:
-				dx = 0;
-				break;
+        switch (direction) {
+        case 8:
+            dx = 0;
+            break;
 
-			case 6:
-				dx = speed;
+        case 6:
+            dx = speed;
+            $("#tileAndObjectsDummy").css("left", parseInt($("#tileAndObjectsDummy").css("left")) - dx);
+            break;
+        case 0:
+            dx = speed;
+            $("#tileAndObjectsDummy").css("top", 0 + parseInt($("#tileAndObjectsDummy").css("top")) - dx);
+            break;
+        case 1:
+            dx = speed;
+            $("#tileAndObjectsDummy").css({
+                left: parseInt($("#tileAndObjectsDummy").css("left")) + dx,
+                top: 0 + parseInt($("#tileAndObjectsDummy").css("top")) - dx
+            });
+            break;
+        case 2:
+            dx = speed;
+            $("#tileAndObjectsDummy").css("left", parseInt($("#tileAndObjectsDummy").css("left")) + dx);
+            break;
+        case 3:
+            dx = speed;
+            $("#tileAndObjectsDummy").css({
+                left: parseInt($("#tileAndObjectsDummy").css("left")) + dx,
+                top: 0 + parseInt($("#tileAndObjectsDummy").css("top")) + dx
+            });
+            break;
+        case 4:
+            dx = speed;
+            $("#tileAndObjectsDummy").css("top", 0 + parseInt($("#tileAndObjectsDummy").css("top")) + dx);
+            break;
 
-				$("#tileAndObjectsDummy").css("left", parseInt($("#tileAndObjectsDummy").css("left")) - dx);
-				break;
+        case 5:
+            dx = speed;
+            $("#tileAndObjectsDummy").css({
+                left: parseInt($("#tileAndObjectsDummy").css("left")) - dx,
+                top: 0 + parseInt($("#tileAndObjectsDummy").css("top")) + dx
+            });
+            break;
+        case 7:
+            dx = speed;
+            $("#tileAndObjectsDummy").css({
+                left: parseInt($("#tileAndObjectsDummy").css("left")) - dx,
+                top: 0 + parseInt($("#tileAndObjectsDummy").css("top")) - dx
+            });
+            break;
 
-			case 0:
-				dx = speed;
-
-				$("#tileAndObjectsDummy").css("top", 0 + parseInt($("#tileAndObjectsDummy").css("top")) - dx);
-				break;
-			case 1:
-				dx = speed;
-
-				$("#tileAndObjectsDummy").css("left", parseInt($("#tileAndObjectsDummy").css("left")) + dx);
-				$("#tileAndObjectsDummy").css("top", 0 + parseInt($("#tileAndObjectsDummy").css("top")) - dx);
-
-				break;
-
-			case 2:
-				dx = speed;
-
-				$("#tileAndObjectsDummy").css("left", parseInt($("#tileAndObjectsDummy").css("left")) + dx);
-				break;
-			case 3:
-				dx = speed;
-
-				$("#tileAndObjectsDummy").css("left", parseInt($("#tileAndObjectsDummy").css("left")) + dx);
-				$("#tileAndObjectsDummy").css("top", 0 + parseInt($("#tileAndObjectsDummy").css("top")) + dx);
-
-				break;
-			case 4:
-				dx = speed;
-
-				$("#tileAndObjectsDummy").css("top", 0 + parseInt($("#tileAndObjectsDummy").css("top")) + dx);
-				break;
-
-			case 5:
-				dx = speed;
-
-				$("#tileAndObjectsDummy").css("left", parseInt($("#tileAndObjectsDummy").css("left")) - dx);
-				$("#tileAndObjectsDummy").css("top", 0 + parseInt($("#tileAndObjectsDummy").css("top")) + dx);
-				break;
-			case 7:
-				dx = speed;
-
-				$("#tileAndObjectsDummy").css("left", parseInt($("#tileAndObjectsDummy").css("left")) - dx);
-				$("#tileAndObjectsDummy").css("top", 0 + parseInt($("#tileAndObjectsDummy").css("top")) - dx);
-				break;
-
-		}
-	}
+        }
+    }
 });
